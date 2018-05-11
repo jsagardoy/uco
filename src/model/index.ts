6aa9ee606dfc148e5e060b05523756e1f4c48dcb
Jehú Sagardoy Martín <jehu@MacBook-Pro-de-Jehu.local>
2018-05-11 17:30:24 +0200

    helper filed name updated to arrayHandler

--- src/model/index.ts
+++ src/model/index.ts
@@ -2,4 +2,4 @@ export {CompanyEntity} from './company';
 export {OperationEntity} from './operation';
 export {PeopleEntity} from './people';
 export {VehicleEntity} from './vehicle';
-export {appendElementToArray,removeElementFromArray,updateElementFromArray} from './helper';
+export {appendElementToArray,removeElementFromArray,updateElementFromArray} from './arrayHandler';
