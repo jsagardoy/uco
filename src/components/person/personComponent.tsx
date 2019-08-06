42404b3ad6592604d77ff9e61560b48de74106e5
Jehú <jehu.sagardoy@gmail.com>
2019-08-06 14:31:19 +0200

    added rutine styles

--- src/components/person/personComponent.tsx
+++ src/components/person/personComponent.tsx
@@ -13,194 +13,185 @@ import { Save, Edit, Cancel, Delete } from '@material-ui/icons';
 import { toast } from 'react-toastify';
 import { css } from 'emotion';
 
-
 interface Props {
-    person: PeopleEntity;
-    showPerson: boolean;
-    addNew: boolean;
-    index?: number;
-    onToggle: (fieldId: string) => void;
-    removeFromList: (fieldId: string, index: number) => void;
-    updateState: (fieldId: string, state: any, idField: string) => void;
+  person: PeopleEntity;
+  showPerson: boolean;
+  addNew: boolean;
+  index?: number;
+  onToggle: (fieldId: string) => void;
+  removeFromList: (fieldId: string, index: number) => void;
+  updateState: (fieldId: string, state: any, idField: string) => void;
 }
 
 interface State {
-    person: PeopleEntity;
+  person: PeopleEntity;
 }
 
 export class PersonComponent extends React.Component<Props, State> {
-    prevState: State;
-    constructor(props: Props) {
-        super(props);
-        this.state = { person: this.props.person };
-        this.prevState = this.state;
+  prevState: State;
+  constructor(props: Props) {
+    super(props);
+    this.state = { person: this.props.person };
+    this.prevState = this.state;
+  }
+  fileSelectedHandler = (fieldName: string, value: File, group: string, fileName: string) => {
+    fileSelectedHandler(fieldName, value, group, fileName, this.state.person, data => {
+      let newState: State = {
+        ...this.state,
+        person: data,
+      };
+      this.setState(newState);
+    });
+  };
+
+  handleChange = (fieldName: string, value: any, group: string) => {
+    this.setState(handleChange(fieldName, value, group, this.state));
+  };
+
+  onEdit = () => {
+    let element: PeopleEntity = this.state.person;
+    element.editable = !element.editable;
+    let newState: State = {
+      ...this.state,
+      person: element,
+    };
+    this.setState(newState);
+  };
+
+  onSave = (value: PeopleEntity) => {
+    let newState: State = {
+      ...this.state,
+    };
+
+    let element: PeopleEntity = {
+      ...value,
+      editable: !this.state.person.editable,
+    };
+    let element2;
+    // ESTO TENGO QUE CAMBIARLO A UN UPDATE EN LUGAR DE ANDAR ELMINANDO EL VACIO
+    if (
+      this.state.person.addressLink.length > 1 &&
+      (this.state.person.addressPic[0].img.data == null || this.state.person.addressPic[0] === null)
+    ) {
+      const newPerson = {
+        ...this.state.person,
+        addressPic: removeElementFromArray(this.state.person.addressPic, item => item.img.data == null),
+      };
+      element2 = {
+        ...element,
+        addressPic: newPerson.addressPic,
+      };
+
+      newState.person = element2;
     }
-    fileSelectedHandler = (fieldName: string, value: File, group: string, fileName: string) => {
-
-        fileSelectedHandler(fieldName, value, group, fileName, this.state.person, (data) => {
-            let newState: State = {
-                ...this.state,
-                person: data
-            }
-            this.setState(newState);
-        })
-    }
-
-
-    handleChange = (fieldName: string, value: any, group: string) => {
-        this.setState(handleChange(fieldName, value, group, this.state));
-    }
-
-    onEdit = () => {
-        let element: PeopleEntity = this.state.person;
-        element.editable = !element.editable;
-        let newState: State = {
-            ...this.state,
-            person: element
-        }
-        this.setState(newState);
-    }
-
-    onSave = (value: PeopleEntity, ) => {
-        let newState: State = {
-            ...this.state
-        }
-
-        let element: PeopleEntity = {
-            ...value,
-            editable: !this.state.person.editable,
-        }
-        let element2;
-        // ESTO TENGO QUE CAMBIARLO A UN UPDATE EN LUGAR DE ANDAR ELMINANDO EL VACIO
-        if (this.state.person.addressLink.length > 1 && (this.state.person.addressPic[0].img.data == null || this.state.person.addressPic[0] === null)) {
-            const newPerson = {
-                ...this.state.person,
-                addressPic: removeElementFromArray(this.state.person.addressPic, (item) => item.img.data == null)
-            }
-            element2 = {
-                ...element,
-                addressPic: newPerson.addressPic
-            }
-
-            newState.person = element2;
-        }
-        if (this.state.person.picsLinks.length > 1 && (this.state.person.picsLinks[0].img.data === null || this.state.person.picsLinks[0] === null)) {
-
-            const newPerson = {
-                ...newState.person,
-                picsLinks: removeElementFromArray(this.state.person.picsLinks, (item) => item.img.data == null)
-            }
-            element2 = {
-                ...element,
-                picsLinks: newPerson.picsLinks
-            }
-        }
-        else {
-            element2 = {
-                ...element
-            }
-        }
-
-        newState = {
-            ...this.state,
-            person: element2
-        }
-        this.setState(newState);
-        this.prevState = newState;//update content for prevState with the saved data
-        this.props.updateState('person', newState.person, 'idPerson');
-        toast.success('Guardado');
+    if (
+      this.state.person.picsLinks.length > 1 &&
+      (this.state.person.picsLinks[0].img.data === null || this.state.person.picsLinks[0] === null)
+    ) {
+      const newPerson = {
+        ...newState.person,
+        picsLinks: removeElementFromArray(this.state.person.picsLinks, item => item.img.data == null),
+      };
+      element2 = {
+        ...element,
+        picsLinks: newPerson.picsLinks,
+      };
+    } else {
+      element2 = {
+        ...element,
+      };
     }
 
-    onCancel = () => {
-        if (this.props.addNew)
-            this.props.removeFromList('person', this.props.index)
-        else {
-            const newState: State = { ...this.prevState };
-            newState.person.editable = false;
-            this.setState(newState);
-        }
+    newState = {
+      ...this.state,
+      person: element2,
+    };
+    this.setState(newState);
+    this.prevState = newState; //update content for prevState with the saved data
+    this.props.updateState('person', newState.person, 'idPerson');
+    toast.success('Guardado');
+  };
+
+  onCancel = () => {
+    if (this.props.addNew) this.props.removeFromList('person', this.props.index);
+    else {
+      const newState: State = { ...this.prevState };
+      newState.person.editable = false;
+      this.setState(newState);
     }
+  };
 
-    //styles
-    divStyle = css`
-            width:80%;
-            margin-left:10%;
-    `;
+  //styles
+  divStyle = css`
+    width: 80%;
+    margin-left: 10%;
+  `;
 
-    imgStyle = css`
-            padding-top: 2%;
-            padding-bottom: 0%;
-            display: block;
-            margin: auto;  
-            max-width: 200px;
-            border-radius:20%;
-    `;
-    
-    cardContentStyle = css`
-        padding-top: 0px;
-    `;
-    //end styles
-    render() {
-        const props = 'img';
-        return (
-            <div className={this.divStyle}>
-                <div id='person' className='person'>
-                    {
-                        this.props.showPerson ?
-                            <Card className='person.card'>
-                                <CardActionArea>
-                                    {
-                                        (this.state.person.picsLinks[0] === null || this.state.person.picsLinks[0].img.data === null) ?
-                                            null
-                                            :
-                                            <CardMedia 
-                                                className={this.imgStyle}
-                                                image={this.state.person.picsLinks[0].img.data}
-                                                title={this.state.person.namePerson}
-                                                component={'img' as 'div'}
-                                            />
-                                    }
-                                    <CardContent className={this.cardContentStyle}>
-                                        <PersonFormComponent
-                                            person={this.state.person}
-                                            handleChange={this.handleChange}
-                                            handlefileSelectorChange={this.fileSelectedHandler}
-                                            editable={this.state.person.editable}
-                                        />
-                                    </CardContent>
-                                </CardActionArea>
-                                <CardActions>
-                                    {
-                                        this.state.person.editable ?
-                                            <>
-                                                <Button onClick={() => this.onSave(this.state.person)}>
-                                                    <Save />
-                                                </Button>
-                                                <Button onClick={(e) => this.onCancel()}>
-                                                    <Cancel />
-                                                </Button>
-                                            </>
-                                            :
-                                            <>
-                                                <Button onClick={(e) => this.onEdit()}>
-                                                    <Edit />
-                                                </Button>
-                                                <Button onClick={(e) => this.props.removeFromList('people', this.props.index)}>
-                                                    <Delete />
-                                                </Button>
-                                            </>
-                                    }
-                                </CardActions>
-                            </Card>
+  imgStyle = css`
+    padding-top: 2%;
+    padding-bottom: 0%;
+    display: block;
+    margin: auto;
+    max-width: 200px;
+    border-radius: 20%;
+  `;
 
-                            :
-                            <>
-                            </>
-                    }
-                </div>
-
-            </div>
-        );
-    }
-
+  cardContentStyle = css`
+    padding-top: 0px;
+  `;
+  //end styles
+  render() {
+    const props = 'img';
+    return (
+      <div className={this.divStyle}>
+        <div id="person" className="person">
+          {this.props.showPerson ? (
+            <Card className="person.card">
+              <CardActionArea>
+                {this.state.person.picsLinks[0] === null || this.state.person.picsLinks[0].img.data === null ? null : (
+                  <CardMedia
+                    className={this.imgStyle}
+                    image={this.state.person.picsLinks[0].img.data}
+                    title={this.state.person.namePerson}
+                    component={'img' as 'div'}
+                  />
+                )}
+                <CardContent className={this.cardContentStyle}>
+                  <PersonFormComponent
+                    person={this.state.person}
+                    handleChange={this.handleChange}
+                    handlefileSelectorChange={this.fileSelectedHandler}
+                    editable={this.state.person.editable}
+                  />
+                </CardContent>
+              </CardActionArea>
+              <CardActions>
+                {this.state.person.editable ? (
+                  <>
+                    <Button onClick={() => this.onSave(this.state.person)}>
+                      <Save />
+                    </Button>
+                    <Button onClick={e => this.onCancel()}>
+                      <Cancel />
+                    </Button>
+                  </>
+                ) : (
+                  <>
+                    <Button onClick={e => this.onEdit()}>
+                      <Edit />
+                    </Button>
+                    <Button onClick={e => this.props.removeFromList('people', this.props.index)}>
+                      <Delete />
+                    </Button>
+                  </>
+                )}
+              </CardActions>
+            </Card>
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
+    );
+  }
 }
--- src/components/rutines/rutinesComponent.tsx
+++ src/components/rutines/rutinesComponent.tsx
@@ -2,13 +2,9 @@ import * as React from 'react';
 
 import { RutineEntity } from '../../model';
 
-import { removeElementFromArray, appendElementToArray, updateElementFromArray, colors } from '../../common'
+import { removeElementFromArray, appendElementToArray, updateElementFromArray, colors } from '../../common';
 import { RutineFormComponent } from '../form';
-import {
-        ListItem,
-        ListItemSecondaryAction,
-        List
-        } from '@material-ui/core';
+import { ListItem, ListItemSecondaryAction, List } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import { createEmptyRutine } from '.';
 import { Edit, Save, Cancel, Delete, Add } from '@material-ui/icons';
@@ -17,196 +13,187 @@ import { css } from 'emotion';
 import { ButtonComponent } from '../Common';
 
 interface Props {
-    rutines: Array<RutineEntity>
-    updateState: (fieldId: string, state: any) => void;
+  rutines: Array<RutineEntity>;
+  updateState: (fieldId: string, state: any) => void;
 }
 interface State {
-    rutines: Array<RutineEntity>;
-    showRutine: boolean;
+  rutines: Array<RutineEntity>;
+  showRutine: boolean;
 }
 
 export class RutinesComponent extends React.Component<Props, State> {
-    prevState: State;
+  public prevState: State;
 
-    constructor(props: Props) {
-        super(props);
-        this.state = {
-            rutines: this.props.rutines,
-            showRutine: false,
-        };
-        this.prevState = this.state;
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      rutines: this.props.rutines,
+      showRutine: false,
+    };
+    this.prevState = this.state;
+  }
+  public removeItem = index => {
+    if (index === -1) { index = this.state.rutines.length || 0; }
+    const newState: State = { ...this.state };
+    newState.rutines = removeElementFromArray(this.state.rutines, item => item === this.state.rutines[index]);
+    this.setState(newState);
+  };
+  public handleSubmit = (index: number) => {
+    if (index === -1) { index = this.state.rutines.length || 0; }
+    const newArray: Array<RutineEntity> = appendElementToArray(this.state.rutines, this.state.rutines[index]);
+    newArray[index].editable = false;
+    const newState = {
+      ...this.state,
+      rutines: newArray,
+    };
+
+    this.setState(newState);
+  };
+  public handleChange = (index: number) => (fieldName: string, value: any, group: string) => {
+    if (index === -1) {
+      index = this.state.rutines.length || 0;
     }
-    removeItem = (index) => {
-        if (index === -1)
-            index = this.state.rutines.length || 0
-        let newState: State = { ...this.state };
-        newState.rutines = removeElementFromArray(this.state.rutines, (item) => item === this.state.rutines[index]);
-        this.setState(newState);
+    const element: RutineEntity = {
+      ...this.state[fieldName][index],
+      data: value,
+    };
+    if (element.editable === null) { element.editable = true; }
+
+    element.editable = true;
+    const newList = updateElementFromArray(this.state[fieldName], element, item => item === this.state[fieldName][index]);
+
+    const newState = {
+      ...this.state,
+      [fieldName]: newList,
+    };
+    this.setState(newState);
+  };
+  public addNewRutine = () => {
+    const element = createEmptyRutine();
+    const newList = appendElementToArray(this.state.rutines, element);
+
+    this.setState({
+      rutines: newList,
+    });
+  };
+  public onEdit = (index: number) => {
+    if (index === -1) { index = this.state.rutines.length || 0; }
+
+    const element: RutineEntity = this.state.rutines[index];
+    element.editable = !element.editable;
+    const newState: State = {
+      ...this.state,
+    };
+    newState.rutines[index] = element;
+
+    this.setState(newState);
+  };
+  public saveRutine = (index: number, value: RutineEntity) => {
+    const newState: State = {
+      ...this.state,
+    };
+    newState.rutines[index].editable = !this.state.rutines[index].editable;
+    this.setState(newState);
+    this.prevState = newState; // update content for prevState with the saved data
+    this.props.updateState('rutines', newState.rutines);
+    toast.success('Guardado');
+  };
+  public onCancel = (index: number) => {
+    if (this.state.rutines.length > this.prevState.rutines.length) {
+      this.removeItem(index);
+    } else {
+      const newState: State = { ...this.prevState };
+      newState.rutines[index].editable = false;
+      this.setState(newState);
     }
-    handleSubmit = (index: number) => {
-        if (index === -1)
-            index = this.state.rutines.length || 0
-        let newArray: Array<RutineEntity> = appendElementToArray(this.state.rutines, this.state.rutines[index]);
-        newArray[index].editable = false;
-        let newState = {
-            ...this.state,
-            rutines: newArray
-        };
+  };
+  public onClickButton = () => {
+    this.setState({ ...this.state, showRutine: !this.state.showRutine });
+  };
 
-        this.setState(newState);
+  // styles
+  public wrapperStyles = css`
+    margin-left: 10%;
+    margin-right: 10%;
+  `;
+  public divStyles = css`
+    width: 100%;
+    border: 1px solid;
+    border-radius: 8px;
+    border-color: ${colors.GREEN};
+    background-color: white;
+  `;
+  public h2Styled = css`
+    width: 100%;
+    margin-top: 2%;
+    margin-bottom: 0.5%;
+    text-align: center;
+    background-color: ${colors.GREEN};
+    color: ${colors.YELLOW};
+    border-radius: 10px;
+  `;
 
-    }
-    handleChange = (index: number) => (fieldName: string, value: any, group: string) => {
-        if (index === -1) {
-            index = this.state.rutines.length || 0
+  public listStyle = css`
+    padding: 0px;
+  `;
 
-        }
-        let element: RutineEntity = {
-            ...this.state[fieldName][index],
-            data: value,
+  // end Styles
 
-        }
-        if (element.editable === null)
-            element.editable = true;
-
-        element.editable = true;
-        let newList = updateElementFromArray(this.state[fieldName], element, (item) => item === this.state[fieldName][index])
-
-        let newState = {
-            ...this.state,
-            [fieldName]: newList
-        }
-        this.setState(newState);
-    }
-    addNewRutine = () => {
-        const element = createEmptyRutine();
-        let newList = appendElementToArray(this.state.rutines, element)
-
-        this.setState(
-            {
-                rutines: newList,
-            }
-        )
-    }
-    onEdit = (index: number) => {
-        if (index === -1)
-            index = this.state.rutines.length || 0
-
-        let element: RutineEntity = this.state.rutines[index];
-        element.editable = !element.editable;
-        let newState: State = {
-            ...this.state,
-        }
-        newState.rutines[index] = element;
-
-        this.setState(newState);
-    }
-    saveRutine = (index: number, value: RutineEntity) => {
-        let newState: State = {
-            ...this.state,
-        }
-        newState.rutines[index].editable = !this.state.rutines[index].editable;
-        this.setState(newState);
-        this.prevState = newState;//update content for prevState with the saved data
-        this.props.updateState('rutines', newState.rutines);
-        toast.success('Guardado');
-    }
-    onCancel = (index: number) => {
-        if (this.state.rutines.length > this.prevState.rutines.length) {
-            this.removeItem(index);
-        }
-        else {
-            const newState: State = { ...this.prevState };
-            newState.rutines[index].editable = false;
-            this.setState(newState);
-        }
-
-
-    }
-    onClickButton = () =>{
-      this.setState({...this.state,
-              showRutine: !this.state.showRutine
-      })
-    }
-
-    //styles
-    wrapperStyles = css`
-        margin-left: 10%;
-        margin-right: 10%;
-    `;
-    divStyles = css`
-        width:100%;
-        border: 1px solid;
-        border-radius: 8px;
-        border-color: ${colors.GREEN};
-        background-color: white;
-    `;
-    h2Styled = css`
-        width: 100%;
-        margin-top: 2%;
-        margin-bottom: 0.5%;
-        text-align: center;
-        background-color: ${colors.GREEN};
-        color: ${colors.YELLOW};
-    `;
-
-    listStyle = css`
-        padding: 0px;
-    `;
-
-    //end Styles
-
-    render() {
-        //const newRutine: RutineEntity = createEmptyRutine();
-        let styleDiv:React.CSSProperties=null;
-        return (
-            <div className={this.wrapperStyles}>
-                <h2 className={this.h2Styled} onClick={this.onClickButton}>Rutinas</h2>
-                {this.state.showRutine?
-                <div className={this.divStyles}>
-                    {
-                        this.state.rutines.map((rutine: RutineEntity, index: number) =>
-
-                            <List key={index} dense={true}>
-                                <ListItem >
-                                    {rutine.editable ?
-                                        <RutineFormComponent key={index}
-                                            rutine={rutine}
-                                            onChange={this.handleChange(index)}
-                                            editable={this.state.rutines[index].editable}
-                                        />
-                                        :
-                                        rutine.data
-                                    }
-                                </ListItem>
-                                <ListItemSecondaryAction >
-                                    {
-                                        rutine.editable ?
-                                            <>
-                                                <Button onClick={(e) => this.saveRutine(index, rutine)}> <Save /></Button>
-                                                <Button onClick={(e) => this.onCancel(index)}><Cancel /></Button>
-                                            </>
-                                            :
-                                            <>
-                                                <Button onClick={(e) => this.onEdit(index)}><Edit /></Button>
-                                                <Button onClick={(e) => this.removeItem(index)}> <Delete /></Button>
-                                            </>
-
-                                    }
-                                </ListItemSecondaryAction>
-                            </List>
-
-
-                        )
-
-                    }
-                    <ButtonComponent text='Añadir una rutina' onClick={(e) => this.addNewRutine()}><Add /></ButtonComponent>
-                </div>
-                :
-                null
-                }
-            </div>
-        )
-    }
-
+  public render() {
+    // const newRutine: RutineEntity = createEmptyRutine();
+    const styleDiv: React.CSSProperties = null;
+    return (
+      <div className={this.wrapperStyles}>
+        <h2 className={this.h2Styled} onClick={this.onClickButton}>
+          Rutinas
+        </h2>
+        {this.state.showRutine ? (
+          <div className={this.divStyles}>
+            {this.state.rutines.map((rutine: RutineEntity, index: number) => (
+              <List key={index} dense={true}>
+                <ListItem>
+                  {rutine.editable ? (
+                    <RutineFormComponent
+                      key={index}
+                      rutine={rutine}
+                      onChange={this.handleChange(index)}
+                      editable={this.state.rutines[index].editable}
+                    />
+                  ) : (
+                    rutine.data
+                  )}
+                </ListItem>
+                <ListItemSecondaryAction>
+                  {rutine.editable ? (
+                    <>
+                      <Button onClick={e => this.saveRutine(index, rutine)}>
+                        {' '}
+                        <Save />
+                      </Button>
+                      <Button onClick={e => this.onCancel(index)}>
+                        <Cancel />
+                      </Button>
+                    </>
+                  ) : (
+                    <>
+                      <Button onClick={e => this.onEdit(index)}>
+                        <Edit />
+                      </Button>
+                      <Button onClick={e => this.removeItem(index)}>
+                        {' '}
+                        <Delete />
+                      </Button>
+                    </>
+                  )}
+                </ListItemSecondaryAction>
+              </List>
+            ))}
+            <ButtonComponent text="Añadir una rutina" onClick={e => this.addNewRutine()}>
+              <Add />
+            </ButtonComponent>
+          </div>
+        ) : null}
+      </div>
+    );
+  }
 }
