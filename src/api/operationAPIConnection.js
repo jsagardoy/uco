df745abe576de50686a2c8c190bbccd1b5fb3423
Jehú <jehu.sagardoy@gmail.com>
2019-09-18 09:17:46 +0200

    trying  new stuff

--- src/api/operationAPIConnection.js
+++ src/api/operationAPIConnection.js
@@ -1,25 +1,27 @@
 import axios from 'axios';
 import {machines} from '../common';
 
-const route = `/api/operations`;
-const url = `https://uco-back.herokuapp.com`;//`http://localhost:4000`;
-const newURL = `${url}${route}`;
+const url = `http://uco-back.herokuapp.com`;//`http://localhost:4000`;
 
 export const getOperations = async () => {
+  const route = `/api/operations`;
+  const newURL = `${url}${route}`;
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token');
-
-
   const operations = await axios.get(newURL);
   return operations.data;
 };
 
 export const putOperation = async data => {
+  const route = `/api/operation`;
+  const newURL = `${url}${route}`;
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token');
   //const url = 'https://localhost:4000/api/operation';
   await axios.put(newURL, data);
 };
 
 export const putPerson = async (id, data) => {
+  const route = `/api/operation`;
+  const newURL = `${url}${route}`;
   axios.defaults.headers.common['Authorization'] = 'Bearer ' + localStorage.getItem('id_token');
   const urlID = `${newURL}/${id}`;
   await axios.put(urlID, data);
